03cabf59a4190c37ff7b6cf0c7cb598eb4f01b75
d0m1s2 <vrubliauskas.domas@gmail.com>
2025-04-08 18:11:47 +0100

    added state machine kind of

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import servoTest
+import anbo_main.pressure as pressure
 import RPi.GPIO as GPIO
 import sys
 import time
 
 while True:
     try:
-        print(servoTest.getPressure())
+        print(pressure.getPressure())
         time.sleep(0.1)
     except (KeyboardInterrupt, SystemExit):
         GPIO.cleanup()
---
+++ stateMachine.py
@@ -0,0 +1,10 @@
+from enum import Enum, auto
+
+class gameStates(Enum):
+    START = auto()
+    TURN_MAGNETO_ON = auto()
+    TURN_VALVE_ON = auto()
+    PUMP_OIL = auto()
+    END = auto()
+
+    
